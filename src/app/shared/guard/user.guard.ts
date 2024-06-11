cb89549c185ba1ad4a94e70e303835e0c88f9e47
NicoPerez22 <nicolas.perez@lubee.com.ar>
2024-06-10 23:22:37 -0300

    Angular 16

--- src/app/shared/guard/user.guard.ts
+++ src/app/shared/guard/user.guard.ts
@@ -1,12 +1,12 @@
 import { CookieService } from 'ngx-cookie-service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuard implements CanActivate {
+export class UserGuard  {
 
   constructor(
     private cookieService: CookieService,
